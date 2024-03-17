2fa1e40b01c433e9750554c4fbf470f79a6ad1fb
aimee rivers <aimee@aimeerivers.com>
2024-03-17 16:10:14 +0100

    test(app): convert all tests over to use supertest

--- test/app.test.ts
+++ test/app.test.ts
@@ -10,7 +10,6 @@ import { app } from "../src/app";
 import { User } from "../src/models/user";
 
 const port = 3001;
-const baseUrl = `http://localhost:${port}`;
 let server: Server;
 
 describe("App", () => {
@@ -44,32 +43,28 @@ describe("App", () => {
   });
 
   it("should say hello world", async () => {
-    const response = await fetch(baseUrl + "/");
-    const body = await response.text();
-    assert.ok(body.includes("Hello World!"));
+    const res = await request(app).get("/");
+    assert.ok(res.text.includes("Hello World!"));
   });
 
   it("should give a 404 when a route is not found", async () => {
-    const response = await fetch(baseUrl + "/aaa");
-    assert.equal(response.status, 404);
+    const res = await request(app).get("/aaa");
+    assert.equal(res.statusCode, 404);
   });
 
   it("should respond to /andre as a POST request", async () => {
-    const response = await fetch(baseUrl + "/andre", { method: "POST" });
-    const body = await response.text();
-    assert.ok(body.includes("potato"));
+    const res = await request(app).post("/andre");
+    assert.ok(res.text.includes("potato"));
   });
 
   it("should say hello to aimee", async () => {
-    const response = await fetch(baseUrl + "/hello/aimee");
-    const body = await response.text();
-    assert.ok(body.includes("Hello aimee!"));
+    const res = await request(app).get("/hello/aimee");
+    assert.ok(res.text.includes("Hello aimee!"));
   });
 
   it("should say hello to zoe", async () => {
-    const response = await fetch(baseUrl + "/hello/zoe");
-    const body = await response.text();
-    assert.ok(body.includes("Hello zoe!"));
+    const res = await request(app).get("/hello/zoe");
+    assert.ok(res.text.includes("Hello zoe!"));
   });
 
   after(async () => {
