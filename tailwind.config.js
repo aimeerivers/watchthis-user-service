978870b7bc85b27afc5a4785d8d0913db94bd5da
aimee rivers <aimee@aimeerivers.com>
2024-06-12 15:52:03 +0200

    feat(style): add tailwind css and some good styles to get started (#36)
    * chore(deps): add tailwind and postcss
    
    * chore(tailwind): initialize tailwind css
    
    * chore(tailwind): initialize postcss
    
    * feat(tailwind): use tailwind css
    
    * chore(scripts): add a script to compile tailwind css
    
    * chore(deps): move tailwind and postcss to dev dependencies
    
    * chore(deps): install postcss-minify
    
    * chore(postcss): use postcss-minify to minify css
    
    * feat(layout): use generated style.css
    
    * chore(prettier): do not format generated style.css file
    
    * chore(tailwind): move tailwind.css to src
    
    * chore(scripts): add tailwind css watch script
    
    * chore(tailwind): use jit and purge to get the right classes
    
    * chore(tailwind): configure tailwind to use content properly
    
    * feat(style): add the basic starter css having imported tailwind
    
    * chore(tailwind): configure tailwind to use darkMode based on media queries
    
    * feat(style): apply basic dark and light colour schemes
    
    * feat(tailwind): apply some sensible tailwind basic classes
    
    * feat(views): improve the views a little bit
    
    * fix(styles): final tweaks to make it look kinda nice
    
    * feat(styles): add a primary button style
    
    * feat(layout): add charset and device scale

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.pug",
+    // './src/**/*.js',
+  ],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
