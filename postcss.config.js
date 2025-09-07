9c697a631b78165687c494c3ccbb371d05e51a8a
aimee rivers <aimee@aimeerivers.com>
2025-09-07 14:52:03 +0200

    chore: Update tailwindcss to 4.x (#154)

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
+import tailwindcss from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
 import postcssMinify from "postcss-minify";
-import tailwindcss from "tailwindcss";
 
 export default {
   plugins: [tailwindcss, autoprefixer, postcssMinify],
